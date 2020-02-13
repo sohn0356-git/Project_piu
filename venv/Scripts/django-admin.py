f571e6727f060968cb703d96eaa544098467db36
sohn0356-git <sohn0356@gmail.com>
2020-02-13 20:13:03 +0900

    pycharm을 이용해서 Django 프로젝트 생성 및 home이라는 앱 생성

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sohn0\Desktop\myFloder\20_1\project_piu\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
